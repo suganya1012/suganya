8d53139b27a403c0af0d94a5fecee504cc327208
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-07-22 15:29:34 +0530

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main()
+{
+long long n;
+int count =o;
+printf("enter an integer:");
+scanf("%||d",&n);
+while(n!=0)
+{
+n/=10;
+++ncount;
+}
+printf ("number of digits :%d,"count);
+}
