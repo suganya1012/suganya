c5dc2f6c8878a436d5395e8f6e2a6d2f3501cd82
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-07-22 15:46:20 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+char c;
+printf("enter a character :");
+scanf("%c",&c);
+if((c>='a'&&c<='z')||(c>'a'&&c<='Z'))
+printf("%c is an alphabet.",());
+else
+printf("%c is not an alphabet."());
+return 0;
+}
