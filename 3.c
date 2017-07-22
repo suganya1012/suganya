9180d29d16856edd2444c890c41e2e5dae46148f
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-07-22 14:59:55 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char c;
+int is lower case vowel,is upper case vowel;
+printf("enter an alphabet:");
+scanf("%c",&c);
+is lower case vowel=((=='a'||c=='e'||c=='i'||c=='o'||c=='u'));
+is upper case vowel =(c=='A'||C=='E'||c=='I'||C=='O'||C=='U');
+(is lower case vowel || is upper case vowel)
+printf("%c is a vowel "');
+else 
+printf ("%c is a constant", );
+return 0;
+}
