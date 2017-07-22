9cb0cac1ee1b607224156eabfb1f6bec49c15262
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-07-22 12:16:17 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int number;
+{
+printf ("enter the integer");
+}
+if (number %2 ==0)
+{
+printf("%d is odd ",number);
+}
+else
+{
+printf ("%d is odd,"number);
+}
+return o;
+}
