d0f91535d4a0058bcd384f8e2eb00a56a3c4ab21
suganya1012 <30343772+suganya1012@users.noreply.github.com>
2017-07-22 15:16:53 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+int x,i=1,sum=0;
+printf("enter the value ");
+scanf("%d",&x);
+printf("first %d numbers are \n"'x);
+for (i=0;i<=x;i++)
+{
+printf('%d',i);
+sum=sum+i;
+}
+printf (\n\nsum=%d,sum);
+return 0;
+}
